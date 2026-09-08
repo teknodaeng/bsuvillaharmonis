d99fb3a66447eac2ddaa77cc9c3db6d00225e913
teknodaeng <teknodaeng@gmail.com>
2026-09-08 17:07:01 +0800

    Delete frontend directory

--- frontend/src/utils/currency.js
+++
@@ -1,8 +0,0 @@
-export const formatRupiah = (amount) => {
-  if (amount === undefined || amount === null || isNaN(amount)) {
-    return "Rp 0";
-  }
-  const numeric = Math.round(Number(amount));
-  const formatted = numeric.toLocaleString("id-ID");
-  return `Rp ${formatted}`;
-};
